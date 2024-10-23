1accf697b64105dac925852b87d445ba0737649d
ncls-brn <nicolas.j.baran@gmail.com>
2024-10-23 16:11:20 +0200

    add class person et def is_major

--- bool_logic.py
+++ bool_logic.py
@@ -1,7 +1,27 @@
+class Person():
+    def is_major(self):
+        return self.age>=18
+    
+toto = Person()
+toto.age=6
+titi= Person()
+titi.age=15
+nana=Person()
+nana.age=16
+nono=Person()
+nono.age=10
+
+a=toto.is_major()
+b= not(titi.is_major())
+c=nana.is_major()
+d=nono.is_major()
+
+"""
 a=1
 b=1
 c=0
 d=0
+"""
 f=not(a and (not b) and (a or b )) or  (not a and b) and not (a or(not b) or (not c ))
 g= not((c or d) and a and not(c) and d and ((not a) and c or (not d)))
 
@@ -12,17 +32,15 @@ s4=(a and b) or c and (not a)or (b and c) and (not b)
 s5= (a or b or c) and (not a or b or c) or (a and b) or c and d
 s6= a or b or (not c) and b or (a or not c ) or not ((not a) or b or ((not a)and c))
 print("------------------")
-print(f)
-print(g)
+print("f=",f)
+print("g=",g)
 print("------------------")
-print(s1)
-print(s2)
+print("s1=",s1)
+print("s2=",s2)
 print("------------------")
-print(bool(s1))
-print(s2)
+print("s3=",s3)
+print("s4=",s4)
 print("------------------")
-print(s3)
-print(s4)
-print("------------------")
-print(s5)
-print(s6)
+print("s5=",s5)
+print("s6=",s6)
+
