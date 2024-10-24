d2a122159351140145efd19a490b77c676c5a336
ncls-brn <nicolas.j.baran@gmail.com>
2024-10-24 15:17:21 +0200

    update files script

--- git_auto.sh
+++ git_auto.sh
@@ -1,9 +1,13 @@
-read -p "Entrez le message de commit:" commit_message
+
+if [ -z "$1" ]; then
+    read -p "Entrez le message de commit : " commit_message
+else
+    commit_message="$1"
+fi
 
 git add .
+git commit -m "$commit_message"
+git push
 
-git commit -m"$commit_message"
+echo "Les changements ont été poussés avec succès !"
 
-git push 
-
-echo " Les changements ont été poussés avec succès!"
