238ddafb3252a527bbf5eaeba8855830820147d0
ncls-brn <nicolas.j.baran@gmail.com>
2024-10-31 14:50:55 +0100

    filter url done + length

--- scrapping.sh
+++ scrapping.sh
@@ -1,2 +1,9 @@
-echo "hello world scrapping" 
+url="https://readi.fi/sitemap.xml" 
+
+pages_return=$(curl -s "$url")
+
+links_of_pages=$(echo "$pages_return" | grep -o 'https://readi\.fi\/asset[^"<]*'> urlscrap.txt)
+
+line_count=$(wc -l < urlscrap.txt)
+echo "Nombre de liens trouvés : $line_count"
 
