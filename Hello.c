9f0ad438559a5061736f6e94cbc4a6427e8c9237
ncls-brn <nicolas.j.baran@gmail.com>
2024-10-31 09:43:44 +0100

    update file hello.c

--- Hello.c
+++ Hello.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-    // Vérifier si un argument a été passé
+    
     if (argc > 1) {
-        // Afficher "Hello" suivi du premier argument
+      
         printf("Hello, %s!\n", argv[1]);
     } else {
-        // Si aucun argument n'est passé, afficher un message d'erreur
+        
         printf("Usage: %s <name>\n", argv[0]);
     }
 
