7c6dc1b9fed1049392c18dd0e69e255e40254ae3
ncls-brn <nicolas.j.baran@gmail.com>
2024-12-10 22:55:05 +0100

    "update hello.s file"

--- hello.s
+++ hello.s
@@ -2,15 +2,15 @@
 .intel_syntax noprefix
 
 _start:
-	mov rax, 1
-	mov rdi, 1
-	lea rsi, [hello]
-	mov rdx, 26
-	syscall
-	
-	mov rax, 60
-	mov rdi, 1
-	syscall
+    mov rax, 1              
+    mov rdi, 1              
+    lea rsi, [rip + hello]  
+    mov rdx, 26             
+    syscall                 
+
+    mov rax, 60             
+    xor rdi, rdi            
+    syscall                
 
 hello:
-	.asciz "Hello, world! world hello!\n"	
+    .asciz "Hello, world! world hello!\n"
